a45e152b28abd6febc2505aadd5efc2b9b83e195
Jyothsna Jairam <mjr.jyothsna@gmail.com>
2025-03-24 17:59:20 +0530

    Revert "prod work"
    This reverts commit 12f2dd6509c1e29f534456062e34819eff2ffb99.

--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -1,14 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
 
-  baseURL : string = environment.url;
+  baseURL : string = "http://localhost:8080/"
 
   constructor(private http: HttpClient) { }
 
